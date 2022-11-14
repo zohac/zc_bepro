ef641b05d34f2d5946f9ab99b171394416a73df7
zohac <simon@jouan.ovh>
2022-11-14 17:08:45 +0100

    feat(header): add scss linter, and header styling

--- header.php
+++ header.php
@@ -28,38 +28,40 @@ use ZcBepro\Includes\Logo;
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'zc_bepro' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title">
-                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                        <?= Logo::getCustomLogo() ?>
-                        <span><?php bloginfo( 'name' ); ?></span>
-                    </a>
-                </h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$zc_bepro_description = get_bloginfo( 'description', 'display' );
-			if ( $zc_bepro_description || is_customize_preview() ) :
-				?>
-				<span class="site-description"><?php echo $zc_bepro_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></span>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+        <div class="container">
+            <div class="site-branding">
+                <?php
+                if ( is_front_page() && is_home() ) :
+                    ?>
+                    <h1 class="site-title">
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                            <?= Logo::getCustomLogo() ?>
+                            <span><?php bloginfo( 'name' ); ?></span>
+                        </a>
+                    </h1>
+                    <?php
+                else :
+                    ?>
+                    <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                    <?php
+                endif;
+                $zc_bepro_description = get_bloginfo( 'description', 'display' );
+                if ( $zc_bepro_description || is_customize_preview() ) :
+                    ?>
+                    <span class="site-description"><?php echo $zc_bepro_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></span>
+                <?php endif; ?>
+            </div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'zc_bepro' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+            <nav id="site-navigation" class="main-navigation">
+                <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'zc_bepro' ); ?></button>
+                <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'menu-1',
+                        'menu_id'        => 'primary-menu',
+                    )
+                );
+                ?>
+            </nav><!-- #site-navigation -->
+        </div><!-- .container -->
 	</header><!-- #masthead -->
